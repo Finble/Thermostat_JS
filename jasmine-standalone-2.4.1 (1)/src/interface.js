ce317aa9c8142fa1b503dcee02a15229293d719e
Tom Pickard <tcpickard94@gmail.com>
2016-02-19 11:44:09 +0000

    Added weather api to get weather data for location
    Location is input via a city name field and passed to the api to
    get the current city name and display temperature accordingly

--- jasmine-standalone-2.4.1 (1)/src/interface.js
+++ jasmine-standalone-2.4.1 (1)/src/interface.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
 
 var thermostat = new Thermostat();
+var city;
 	displayTemperature();
 	displayColor();
 
 $('.temperature-increase').click(function(){
 	thermostat.increaseTemperature();
+    console.log(thermostat.temperature);
 	displayTemperature();
 	displayColor();
 });
@@ -31,6 +33,22 @@ $('.toggle-power-saving-mode').click(function(){
 	displayTemperature();
 	displayColor();
 });
+
+$(".select-city").click(function(){
+    city = $(".city-input").val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=a3d9eb01d4de82b9b8d0849ef604dbed', function(data) {
+    $("#current-temperature").text(data.main.temp);
+    $("#current-city").text(data.name);
+    });
+});
+
+$.get('http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=a3d9eb01d4de82b9b8d0849ef604dbed', function(data) {
+    $("#current-temperature").text(data.main.temp);
+    $("#current-city").text(data.name);
+
+});
+
+
 
 function displayColor(){
 	$('.display-temperature').css('background-color', thermostat.displayTemperature);
